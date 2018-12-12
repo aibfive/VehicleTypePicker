0d1709cc87631dfcc9ea3a5935f39ca801b37f12
aibfive <1456574276@qq.com>
2018-12-12 18:28:34 +0800

    1、Commit Message；

--- app/src/main/java/com/sandu/vehicletypepicker/picker/view/SideIndexBar.java
+++ app/src/main/java/com/sandu/vehicletypepicker/picker/view/SideIndexBar.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -21,8 +26,10 @@ import com.sandu.vehicletypepicker.R;
  */
 public class SideIndexBar extends View {
 
-    private int textSize = 14;
-    private int textColor = Color.BLACK;
+    private OnIndexSelectChangedListener listener;
+
+    private float textSize = 16;
+    private int textNormalColor = Color.BLACK;
     private int textSelectedColor = Color.RED;
     private String[] indexs = new String[]{
             "#", "A", "B", "C", "D", "E", "F", "G", "H",
@@ -30,46 +37,144 @@ public class SideIndexBar extends View {
             "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     };
 
+    private int width, height;
+    private float divider;
+    private Paint normalPaint, selectedPaint;
+
+    private int currentIndex = -1;
+
     public SideIndexBar(Context context) {
         super(context);
         initAttribute(null, 0);
+        initPaint();
     }
 
     public SideIndexBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initAttribute(attrs, 0);
+        initPaint();
     }
 
     public SideIndexBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initAttribute(attrs, defStyleAttr);
+        initPaint();
     }
 
     private void initAttribute(AttributeSet attrs, int defStyle){
         TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.SideIndexBar, defStyle, 0);
-        textSize = array.getDimensionPixelSize(R.styleable.SideIndexBar_textSize, textSize);
-        textColor = array.getColor(R.styleable.SideIndexBar_textColor, textColor);
+        textSize = array.getDimension(R.styleable.SideIndexBar_textSize, textSize);
+        textNormalColor = array.getColor(R.styleable.SideIndexBar_textNormalColor, textNormalColor);
         textSelectedColor = array.getColor(R.styleable.SideIndexBar_textSelectedColor, textSelectedColor);
-        indexs = (String[])array.getTextArray(R.styleable.SideIndexBar_indexs);
+        String[] indexs = (String[])array.getTextArray(R.styleable.SideIndexBar_indexs);
+        if(indexs != null){
+            this.indexs = indexs;
+        }
         array.recycle();
     }
 
+    private void initPaint(){
+        normalPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        normalPaint.setTextSize(textSize);
+        normalPaint.setColor(textNormalColor);
+
+        selectedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        selectedPaint.setTextSize(textSize);
+        selectedPaint.setColor(textSelectedColor);
+    }
+
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        Log.i("GG", "w-->"+w);
-        Log.i("GG", "h-->"+h);
-        Log.i("GG", "oldw-->"+oldw);
-        Log.i("GG", "oldh-->"+oldh);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+        divider = (height - (textSize * indexs.length)) / (indexs.length + 1);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        int indexLength = indexs.length;
+
+        for(int i = 0; i < indexLength; i++){
+            float textWidth = normalPaint.measureText(indexs[i]);
+            canvas.drawText(
+                    indexs[i], (width - textWidth) / 2.0f,
+                    (i + 1) * (divider + textSize), i == currentIndex ? selectedPaint : normalPaint
+            );
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float y = event.getY();
+                int index = (int)(y / (divider + textSize));
+                if(containYInArea(y, index)){
+                    if(index != currentIndex){
+                        if(listener != null){
+                            listener.onSelectChanged(indexs[index], index);
+                        }
+                        currentIndex = index;
+                        invalidate();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                currentIndex = -1;
+                invalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
+
+    /**
+     * 判断触及点是否在索引区域内
+     * @param y
+     * @param index
+     * @return
+     */
+    private boolean containYInArea(float y, int index){
+        /**
+         * (index + 1) * divider + index * textSize--索引字符串的top位置
+         * (index + 1) * (divider + textSize)--索引字符串的bottom位置
+         * divider / 2.0f--之所以对这个值进行减加是为了扩大索引的点击有效范围
+         *
+         * 如理解困难，可以绘画矩形RectF显示，这样比较明显，方便。
+         */
+        if(y >= (index + 1) * divider + index * textSize - divider / 2.0f
+                && y <= (index + 1) * (divider + textSize) +  divider / 2.0f){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 索引选择改变监听器
+     */
+    public interface OnIndexSelectChangedListener{
+        /**
+         * 索引选择改变触发
+         * @param index
+         * @param position
+         */
+        void onSelectChanged(String index, int position);
+    }
+
+    /**
+     * 设置索引选择改变监听器
+     * @param listener
+     */
+    public void setOnIndexSelectChangedListener(OnIndexSelectChangedListener listener){
+        this.listener = listener;
+    }
+
 }
--- app/src/main/java/com/sandu/vehicletypepicker/picker/view/VehicleTypePicker.java
+++ app/src/main/java/com/sandu/vehicletypepicker/picker/view/VehicleTypePicker.java
@@ -1,10 +1,13 @@
 package com.sandu.vehicletypepicker.picker.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+
+import com.sandu.vehicletypepicker.R;
 
 /**
  * VehicleTypePicker
@@ -14,15 +17,43 @@ import android.widget.FrameLayout;
  */
 public class VehicleTypePicker extends FrameLayout{
 
+    private float indexBarWidth;
+    private float indexSize;
+    private int indexNormalColor;
+    private int indexSelectedColor;
+    private String[] indexs = new String[]{
+            "#", "A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+            "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    };
+
     public VehicleTypePicker(@NonNull Context context) {
         super(context);
+        initAttribute(null, 0);
     }
 
     public VehicleTypePicker(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initAttribute(attrs, 0);
     }
 
     public VehicleTypePicker(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initAttribute(attrs, defStyleAttr);
     }
+
+    private void initAttribute(AttributeSet attrs, int defStyle){
+        TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.VehicleTypePicker, defStyle, 0);
+        indexBarWidth = array.getDimension(R.styleable.VehicleTypePicker_indexBarWidth, indexBarWidth);
+        indexSize = array.getDimension(R.styleable.VehicleTypePicker_indexSize, indexSize);
+        indexNormalColor = array.getColor(R.styleable.VehicleTypePicker_indexNormalColor, indexNormalColor);
+        indexSelectedColor = array.getColor(R.styleable.VehicleTypePicker_indexSelectedColor, indexSelectedColor);
+        String[] indexs = (String[])array.getTextArray(R.styleable.VehicleTypePicker_indexs);
+        if(indexs != null){
+            this.indexs = indexs;
+        }
+        array.recycle();
+    }
+
+
 }
